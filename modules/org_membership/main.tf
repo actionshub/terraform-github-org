b519fdbffaf0ce4fa7e49bfa64a86b303d91a311
Jason Field <jason@avon-lea.co.uk>
2020-01-19 18:03:12 +0000

    Create the Bats repository

--- modules/org_membership/main.tf
+++ modules/org_membership/main.tf
@@ -2,3 +2,18 @@ resource "github_membership" "Xorima" {
   username = "Xorima"
   role     = "admin"
 }
+
+resource "github_membership" "tas50" {
+  username = "tas50"
+  role     = "admin"
+}
+
+resource "github_membership" "rshade" {
+  username = "rshade"
+  role     = "member"
+}
+
+resource "github_membership" "PowerSchill" {
+  username = "PowerSchill"
+  role     = "member"
+}
