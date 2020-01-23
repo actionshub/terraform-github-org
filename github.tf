99fcca47e1ed2fd7ee87134e2d415a98ab733790
Jason Field <jason@avon-lea.co.uk>
2020-01-23 22:16:30 +0000

    Formatting

--- github.tf
+++ github.tf
@@ -1,8 +1,8 @@
 module "github" {
-  source        = "./modules/repository"
-  name          = ".github"
-  cookbook_team = github_team.admins.id
-  homepage_url  = "https://help.github.com/en/articles/creating-a-default-community-health-file-for-your-organization"
-  description   = "Community Health Files for Sous-Chefs repos"
+  source            = "./modules/repository"
+  name              = ".github"
+  cookbook_team     = github_team.admins.id
+  homepage_url      = "https://help.github.com/en/articles/creating-a-default-community-health-file-for-your-organization"
+  description       = "Community Health Files for Sous-Chefs repos"
   additional_topics = ["community-health"]
 }
