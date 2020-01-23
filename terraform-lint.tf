cf6ca154f8022e3458aab53f1caf6ee149b5935d
Jason Field <jason@avon-lea.co.uk>
2020-01-23 21:26:52 +0000

    Create the Terraform Lint action
    Signed-off-by: Jason Field <jason@avon-lea.co.uk>

---
+++ terraform-lint.tf
@@ -0,0 +1,17 @@
+module "terraform-lint" {
+  source        = "./modules/repository"
+  name          = "terraform-lint"
+  cookbook_team = github_team.terraform-lint.id
+}
+
+resource "github_team" "terraform-lint" {
+  name        = "terraform-lint"
+  description = "terraform-lint Action Maintainers"
+  privacy     = "closed"
+}
+
+resource "github_team_membership" "terraform-lint-member-Xorima" {
+  team_id  = github_team.terraform-lint.id
+  username = "Xorima"
+  role     = "member"
+}
